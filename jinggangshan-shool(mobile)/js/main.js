bd7c12a98b589e0840da0a6ab1e4f176772bc57e
qifei <1804219567@qq.com>
2017-09-26 13:57:24 +0800

    完成移动端

--- jinggangshan-shool(mobile)/js/main.js
+++ jinggangshan-shool(mobile)/js/main.js
@@ -75,9 +75,28 @@ $(document).ready(function () {
 			        }
         
 		  });
-		  
-		 
-			
-
-  
+ 
 })
+
+
+
+//点击到顶部
+
+
+/**超出多少显示**/
+ $(window).scroll(function(){
+     var sc=$(window).scrollTop();
+     var rwidth=$(window).width()
+         if(sc>0){
+    $("#goTopBtn").css("display","block");
+    $("#goTopBtn").css("right",0.375+"rem")
+    }else{
+   $("#goTopBtn").css("display","none");
+    }
+    })
+/****/
+   $("#goTopBtn").click(function(){
+    var sc=$(window).scrollTop();
+   $('body,html').animate({scrollTop:0},500);
+    })  
+    
